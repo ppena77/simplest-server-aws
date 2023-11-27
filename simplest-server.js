b8ae1e10091602ece6df30f2f502312ebc9040dd
ppena77 <ppena77@alumno.uned.es>
2023-11-27 21:21:40 +0100

    Simplest server listening in port 3666

---
+++ simplest-server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+const port = 3666;
+
+app.get('/', (req, res) => res.send('Hello world'));
+
+app.listen(port, () => console.log('Listenging in port 3000'));
